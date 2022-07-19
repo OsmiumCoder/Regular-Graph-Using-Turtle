14364afe9eedf081cc303a30f14969f329a11638
Jonathon Meney <79815359+OsmiumCoder@users.noreply.github.com>
2022-07-19 13:28:22 -0300

    began commenting and other documentation

--- main.py
+++ main.py
@@ -1,56 +1,73 @@
-import turtle
+from turtle import Turtle, screensize, getscreen, done
 import math
 
 
-def graph(n):
+def draw_graph(n):
+    # radius and circumference of the circle which the regular graph will be contained in
+    rad = 250
+    circumference = 2 * math.pi * rad
+
+    screensize(1000, 1000)  # define the screen size
+    screen = getscreen()  # open the window for the user
+    screen.clear()  # delete arrow sprite
+
+    turtle_pen = Turtle()  # init a turtle to draw with
+    # turtle_pen.hideturtle()  # hide the turtle while drawing
+    turtle_pen.speed(10)  # max speed
+    turtle_pen.penup()  # up to not draw straight line
+
+    # go to x coordinate equal to the radius of the circle the graph is contained in
+    # y coordinate 0
+    turtle_pen.goto(rad, 0)
+
+    # pen down and prepare to draw
+    turtle_pen.pendown()
+
+    # rotate 90 degrees to tangent the circle
+    turtle_pen.left(90)
+
+    # list of positions of all vertices
+    # which must still be travelled to
+    positions = []
+
     for line in range(1, n):
         angle = 180 / n
         arc = circumference / n * line
         chord = 2 * rad * math.sin(arc/(2*rad))
 
-        t.left(angle)
-        t.forward(chord)
-        position.append(t.position())
-        t.penup()
-        t.backward(chord)
-        t.pendown()
+        turtle_pen.left(angle)
+        turtle_pen.forward(chord)
+        positions.append(turtle_pen.position())
+        turtle_pen.penup()
+        turtle_pen.backward(chord)
+        turtle_pen.pendown()
 
-    for nextVertex in range(0, len(position)):
-        t.penup()
-        t.goto(position[0])
-        for nextLine in range(0, len(position)):
-            t.pendown()
-            t.goto(position[nextLine])
-            t.penup()
-            if len(position) > 2:
-                t.goto(position[0])
-                t.pendown()
+    for nextVertex in range(0, len(positions)):
+        turtle_pen.penup()
+        turtle_pen.goto(positions[0])
+        for nextLine in range(0, len(positions)):
+            turtle_pen.pendown()
+            turtle_pen.goto(positions[nextLine])
+            turtle_pen.penup()
+            if len(positions) > 2:
+                turtle_pen.goto(positions[0])
+                turtle_pen.pendown()
 
-        del position[0]
+        del positions[0]
 
 
-numOfVertex = int(input("Number of vertices: ") or "3")
-if numOfVertex < 3:
-    numOfVertex = 3
+def main():
+    # ask the user for the number of vertices their regular graph should have
+    num_of_vertex = int(input("Number of vertices: ") or "3")
 
-rad = 250
-circumference = 2 * math.pi * rad
+    # default the number of vertices to three
+    if num_of_vertex < 3:
+        num_of_vertex = 3
 
-turtle.screensize(1000, 1000)
-s = turtle.getscreen()
-s.clear()
+    draw_graph(num_of_vertex)
+    print("Done")
+    done()
 
-t = turtle.Turtle()
-t.hideturtle()
-t.speed(100000)
-t.penup()
-t.goto(rad, 0)
 
-t.pendown()
-t.left(90)
-
-position = []
-
-graph(numOfVertex)
-print("Done")
-turtle.done()
+if __name__ == "__main__":
+    main()
