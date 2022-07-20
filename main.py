4e429366d4e45624b5c25e44678e37366e2e05f3
Jonathon Meney <79815359+OsmiumCoder@users.noreply.github.com>
2022-07-20 13:52:21 -0300

    comments and type hints

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
-from turtle import Turtle, screensize, getscreen, done
-import math
+from turtle import Turtle, screensize, getscreen, done, TurtleScreen
+from math import pi, sin
 
 
-def draw_graph(vertices):
+def draw_graph(vertices: int) -> None:
     # radius and circumference of the circle which the regular graph will be contained in
-    rad = 250
-    circumference = 2 * math.pi * rad
+    rad: int = 250
+    circumference: float = 2 * pi * rad
 
     screensize(1000, 1000)  # define the screen size
-    screen = getscreen()  # open the window for the user
+    screen: TurtleScreen = getscreen()  # open the window for the user
     screen.clear()  # delete arrow sprite
 
-    turtle_pen = Turtle()  # init a turtle to draw with
+    turtle_pen: Turtle = Turtle()  # init a turtle to draw with
     turtle_pen.hideturtle()  # hide the turtle while drawing
     turtle_pen.speed(10)  # max speed
-    turtle_pen.penup()  # up to not draw straight line
+    turtle_pen.penup()  # up to not draw any lines during setup
 
     # go to x coordinate equal to the radius of the circle the graph is contained in
     # y coordinate 0
@@ -28,11 +28,11 @@ def draw_graph(vertices):
 
     # list of positions of all vertices
     # which must still be travelled to
-    positions = []
+    positions: list[tuple[float, float]] = []
 
     # calculate the degrees to rotate
     # each time we draw a new line
-    angle = 180 / vertices
+    angle: float = 180 / vertices
 
     # ALL INITIAL VERTICES
     # draw all lines from initial point
@@ -40,11 +40,11 @@ def draw_graph(vertices):
     for line in range(1, vertices):
         # calculate the arc length based on number of vertices
         # and based on what line we are drawing
-        arc = circumference / vertices * line
+        arc: float = circumference / vertices * line
 
         # calculate chord length, or length of line
         # see also: https://en.wikipedia.org/wiki/Chord_(geometry)
-        chord = 2 * rad * math.sin(arc / (2 * rad))
+        chord: float = 2 * rad * sin(arc / (2 * rad))
 
         # rotate to the angle the next chord is at
         # and draw the chord
@@ -64,10 +64,12 @@ def draw_graph(vertices):
     # CONNECT ALL VERTICES TO EACH OTHER
     # move to the next vertex
     # and draw all its chords
-    for vertex in range(0, len(positions)):
+    for _ in range(0, len(positions)):
+        current_vertex: tuple[float, float] = positions[0]
+
         # pen up and move to next vertex in queue
         turtle_pen.penup()
-        turtle_pen.goto(positions[0])
+        turtle_pen.goto(current_vertex)
 
         # draw all chords for current vertex
         for next_vertex in positions:
@@ -77,22 +79,25 @@ def draw_graph(vertices):
 
             # pen up and return to current vertex
             turtle_pen.penup()
-            turtle_pen.goto(positions[0])
+            turtle_pen.goto(current_vertex)
 
+        # delete current vertex after all lines drawn from it
+        # used to avoid visiting the same vertex twice
         del positions[0]
 
 
-def main():
+def main() -> None:
     # ask the user for the number of vertices their regular graph should have
-    num_of_vertex = int(input("Number of vertices: ") or "3")
+    num_of_vertex: int = int(input("Number of vertices: ") or "3")
 
     # default the number of vertices to three
     if num_of_vertex < 3:
-        num_of_vertex = 3
+        num_of_vertex: int = 3
 
+    # run regular graph algorithm
     draw_graph(num_of_vertex)
-    print("Done")
-    done()
+    
+    done()  # turtle event loop
 
 
 if __name__ == "__main__":
